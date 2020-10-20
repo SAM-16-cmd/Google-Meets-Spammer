7e45fd6c714c63a0f2a04c2fc13af506ba0540e4
SockRocks <67668693+SockRocks@users.noreply.github.com>
2020-10-19 20:17:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,6 @@ from art import text2art
 from platform import system
 
 '''Version: 1.0.3.1'''
-#Remove this when publishing code and when releasing a fully public version
-generic_email = 'cornboyloveshiscorn@gmail.com'
-generic_pass = 'Qay8bwEXkgEzUJF'
 username = environ['USERNAME']
 opperSystem = system()
 nonWindows = False
